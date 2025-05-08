2e55773ae58006b3451366b7ecc2cfc8ab8cc21a
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-05-09 06:01:31 +0800

    feat: add `zh-CN` & `zh-TW`

--- dist/macos-font.user.js
+++ dist/macos-font.user.js
@@ -1,16 +1,20 @@
 // ==UserScript==
-// @name             macOS Font
-// @version          1.0.0
-// @description      Enable antialiased fonts for macOS.
-// @author           三咲智子 Kevin Deng <sxzz@sxzz.moe>
-// @homepage         https://github.com/sxzz/userscripts
-// @supportURL       https://github.com/sxzz/userscripts/issues
-// @license          MIT
-// @contributionURL  https://github.com/sponsors/sxzz
-// @namespace        https://github.com/sxzz/userscripts/blob/main/dist/macos-font.user.js
-// @run-at           document-start
-// @include          *
-// @downloadURL      https://github.com/sxzz/userscripts/raw/refs/heads/main/dist/macos-font.user.js
+// @name                macOS Antialiased Font
+// @@name:zh-CN         macOS 抗锯齿字体
+// @@name:zh-TW         macOS 抗鋸齒字型
+// @version             1.0.0
+// @description         Enable antialiased fonts for macOS.
+// @@description:zh-CN  启用 macOS 的抗锯齿字体。
+// @@description:zh-TW  啟用 macOS 的抗鋸齒字型。
+// @author              三咲智子 Kevin Deng <sxzz@sxzz.moe>
+// @homepage            https://github.com/sxzz/userscripts
+// @supportURL          https://github.com/sxzz/userscripts/issues
+// @license             MIT
+// @contributionURL     https://github.com/sponsors/sxzz
+// @namespace           https://github.com/sxzz/userscripts/blob/main/dist/macos-font.user.js
+// @run-at              document-start
+// @include             *
+// @downloadURL         https://github.com/sxzz/userscripts/raw/refs/heads/main/dist/macos-font.user.js
 // ==/UserScript==
 (function() {
 
--- tsdown.config.ts
+++ tsdown.config.ts
@@ -24,9 +24,15 @@ export default defineConfig([
       ...sharedConfig.outputOptions,
       banner: generateBanner({
         name: 'Force Enable Vue Devtools',
-        version: '0.0.0',
+        '@name:zh-CN': '强制启用 Vue Devtools',
+        '@name:zh-TW': '強制啟用 Vue Devtools',
+        version: '1.0.0',
         description:
           'Force enable Vue Devtools for production-build apps of Vue 2 or Vue 3.',
+        '@description:zh-CN':
+          '强制启用 Vue Devtools，适用于 Vue 2 或 Vue 3 的生产环境构建应用。',
+        '@description:zh-TW':
+          '強制啟用 Vue Devtools，適用於 Vue 2 或 Vue 3 的生產環境構建應用。',
         ...sharedBanner,
         namespace:
           'https://github.com/sxzz/userscripts/blob/main/dist/vue-devtools.user.js',
@@ -43,9 +49,13 @@ export default defineConfig([
     outputOptions: {
       ...sharedConfig.outputOptions,
       banner: generateBanner({
-        name: 'macOS Font',
+        name: 'macOS Antialiased Font',
+        '@name:zh-CN': 'macOS 抗锯齿字体',
+        '@name:zh-TW': 'macOS 抗鋸齒字型',
         version: '1.0.0',
         description: 'Enable antialiased fonts for macOS.',
+        '@description:zh-CN': '启用 macOS 的抗锯齿字体。',
+        '@description:zh-TW': '啟用 macOS 的抗鋸齒字型。',
         ...sharedBanner,
         namespace:
           'https://github.com/sxzz/userscripts/blob/main/dist/macos-font.user.js',
