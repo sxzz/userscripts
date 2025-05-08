e6d07d6ebb1a1f2860e55cb887abc7ed0cf78f31
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-05-09 05:36:20 +0800

    refactor: generate banner

--- dist/vue-devtools.user.js
+++ dist/vue-devtools.user.js
@@ -1,18 +1,17 @@
 // ==UserScript==
-// @name              Force Enable Vue Devtools
-// @version           0.0.0
-// @author            三咲智子 Kevin Deng <sxzz@sxzz.moe>
-// @description       Force enable Vue Devtools for production-build apps of Vue 2 or Vue 3.
-// @homepage          https://github.com/sxzz/userscripts
-// @supportURL        https://github.com/sxzz/userscripts/issues
-// @namespace         https://sxzz.dev
-// @license           MIT
-// @run-at            document-start
-// @noframes
-// @include           /^.*$/
-// @downloadURL       https://github.com/sxzz/userscripts/raw/refs/heads/main/dist/vue-devtools.user.js
+// @name         Force Enable Vue Devtools
+// @version      0.0.0
+// @author       三咲智子 Kevin Deng <sxzz@sxzz.moe>
+// @description  Force enable Vue Devtools for production-build apps of Vue 2 or Vue 3.
+// @homepage     https://github.com/sxzz/userscripts
+// @supportURL   https://github.com/sxzz/userscripts/issues
+// @namespace    https://sxzz.dev
+// @license      MIT
+// @run-at       document-start
+// @noframes     
+// @include      *
+// @downloadURL  https://github.com/sxzz/userscripts/raw/refs/heads/main/dist/vue-devtools.user.js
 // ==/UserScript==
-//
 (function() {
 
 "use strict";
--- tsdown.config.ts
+++ tsdown.config.ts
@@ -9,21 +9,32 @@ export default defineConfig({
 
   outputOptions: {
     entryFileNames: '[name].user.js',
-    banner: `
-// ==UserScript==
-// @name              Force Enable Vue Devtools
-// @version           0.0.0
-// @author            三咲智子 Kevin Deng <sxzz@sxzz.moe>
-// @description       Force enable Vue Devtools for production-build apps of Vue 2 or Vue 3.
-// @homepage          https://github.com/sxzz/userscripts
-// @supportURL        https://github.com/sxzz/userscripts/issues
-// @namespace         https://sxzz.dev
-// @license           MIT
-// @run-at            document-start
-// @noframes
-// @include           /^.*$/
-// @downloadURL       https://github.com/sxzz/userscripts/raw/refs/heads/main/dist/vue-devtools.user.js
-// ==/UserScript==
-// `.trim(),
+    banner: generateBanner({
+      name: 'Force Enable Vue Devtools',
+      version: '0.0.0',
+      author: '三咲智子 Kevin Deng <sxzz@sxzz.moe>',
+      description:
+        'Force enable Vue Devtools for production-build apps of Vue 2 or Vue 3.',
+      homepage: 'https://github.com/sxzz/userscripts',
+      supportURL: 'https://github.com/sxzz/userscripts/issues',
+      namespace: 'https://sxzz.dev',
+      license: 'MIT',
+      'run-at': 'document-start',
+      noframes: '',
+      include: '*',
+      downloadURL: `https://github.com/sxzz/userscripts/raw/refs/heads/main/dist/vue-devtools.user.js`,
+    }),
   },
 })
+
+function generateBanner(properties: Record<string, string>) {
+  const maxLength = Math.max(
+    ...Object.keys(properties).map((key) => key.length),
+  )
+  const lines = Object.entries(properties).map(([key, value]) => {
+    return `// @${key.padEnd(maxLength + 1)} ${value}`
+  })
+  return `// ==UserScript==
+${lines.join('\n')}
+// ==/UserScript==`
+}
